ff1b138c6a8aada374eb3256e57e15de34493553
Cristian Tincu <cristian@rupe.re>
2012-05-29 02:53:08 +0300

    Fixed CRLF.

---
+++ test/euh.test.browser.js
@@ -0,0 +1,44 @@
+;(function(context, undefined) {
+  "use strict";
+
+  ø
+    .log("euh.js ", ø.VERSION)
+    .ln()
+    .ast()
+    .ln()
+  ;
+
+  ø
+    .log("Captain’s log, stardate 2258.42.")
+    .warn("Siehe, ich bin das, was sich immer selber überwinden muß.")
+    .error("Errare humanum est, sed perseverare diabolicum.")
+    .ln()
+    .sep()
+    .ln()
+  ;
+
+  ø
+    .fyi("ø.enabled: ", ø.enabled)
+    .par("context: ", context)
+    .pil("this: ", this)
+    .ln()
+    .wtf("ø.wtf.prototype: ", ø.wtf.prototype)
+    .wat("ø.wat.prototype: ", ø.wat.prototype)
+    .wut("ø.wut.prototype: ", ø.wut.prototype)
+    .br()
+    .omg("ø.omd: ", ø.omd)
+    .omd("ø.omg: ", ø.omg)
+    .nl()
+  ;
+
+  var noConflict = ø.noConflict();
+  noConflict
+    .par("ø: " + ø)
+    .ln()
+  ;
+
+  if (noConflict.consoleExists === false) {
+    alert(noConflict.logs);
+  }
+
+}).call(this, this);
---
+++ test/euh.test.node.js
@@ -0,0 +1,36 @@
+;(function(context, undefined) {
+  "use strict";
+
+  var ø = require("../euh.js").ø;
+
+  ø
+    .log("euh.js ", ø.VERSION)
+    .ln()
+    .ast()
+    .ln()
+  ;
+
+  ø
+    .log("Captain’s log, stardate 2258.42.")
+    .warn("Siehe, ich bin das, was sich immer selber überwinden muß.")
+    .error("Errare humanum est, sed perseverare diabolicum.")
+    .ln()
+    .sep()
+    .ln()
+  ;
+
+  ø
+    .fyi("ø.enabled: ", ø.enabled)
+    .par("context: ", context)
+    .pil("this: ", this)
+    .ln()
+    .wtf("ø.wtf.prototype: ", ø.wtf.prototype)
+    .wat("ø.wat.prototype: ", ø.wat.prototype)
+    .wut("ø.wut.prototype: ", ø.wut.prototype)
+    .br()
+    .omg("ø.omd: ", ø.omd)
+    .omd("ø.omg: ", ø.omg)
+    .nl()
+  ;
+
+}).call(this, this);
